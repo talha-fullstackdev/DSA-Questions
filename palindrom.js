8f7a9d98cd940d73bc69ab4c9f1758305f82e1f5
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-12 16:32:40 -0800

    practice palindrom

--- palindrom.js
+++ palindrom.js
@@ -11,20 +11,37 @@
 
 //   return "palindrom";
 // };
+///////////////////////// practice
 // const check = checkIfPalindrome("level")
 // console.log(check)
-const checkPalindrom = (input) => {
-  const string = input.toString();
-  let left = 0;
-  let right = string.length - 1;
-  while (left < right) {
-    if (string[left] != string[right]) {
-      return "not palindrom";
+// const checkPalindrom = (input) => {
+//   const string = input.toString();
+//   let left = 0;
+//   let right = string.length - 1;
+//   while (left < right) {
+//     if (string[left] != string[right]) {
+//       return "not palindrom";
+//     }
+//     left++;
+//     right--;
+//   }
+//   return "palindrom";
+// };
+// const check = checkPalindrom(1241);
+// console.log(check);
+//////////////////practice
+const checkPalin =(input)=>{
+  const string = input.toString()
+  let left = 0
+  let right = string.length-1
+  while(left<right){
+    if(string[left]!==string[right]){
+      return "not palindrom"
     }
-    left++;
-    right--;
+    left++
+    right--
   }
-  return "palindrom";
-};
-const check = checkPalindrom(1241);
-console.log(check);
+  return "palindrom"
+}
+const check = checkPalin("aba")
+console.log(check)
