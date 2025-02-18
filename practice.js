2ca9d70bcbd78ae43fdffa89ceb3f70bc2778860
talha-fullstackdev <talhafullstack786@gmail.com>
2025-02-17 20:49:10 -0800

    updated practice.js

--- practice.js
+++ practice.js
@@ -15,26 +15,42 @@
 // let check = isPalindrom("")
 // console.log(check)
 ////////////////////////////////////////////////////////
-const isSorted = (array) => {
-  let assending = true;
-  let descending = true;
-  if (array.length === 0) {
-    return "array is empty";
+// const isSorted = (array) => {
+//   let assending = true;
+//   let descending = true;
+//   if (array.length === 0) {
+//     return "array is empty";
+//   }
+//   if (array.length < 2) {
+//     return "need minimum 2 array element to check for sorting";
+//   }
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] < array[i - 1]) {
+//       assending = false;
+//     }
+//     if (array[i] > array[i - 1]) {
+//       descending = false;
+//     }
+//   }
+//   if (assending) return "sorted in assending order";
+//   if (descending) return "sorted in descending order";
+//   return "array do not have any order";
+// };
+// let check = isSorted([2, 1]);
+// console.log(check);
+let array = [1,2,3,4,5,6,7,8];
+let even = 0;
+let odd = 0;
+if(array.length==0){
+    console.log("empty array")
+    return
+}
+for (let i = 0; i < array.length; i++) {
+  if (array[i] % 2 === 0) {
+    even += 1;
+  } else {
+    odd += 1;
   }
-  if (array.length < 2) {
-    return "need minimum 2 array element to check for sorting";
-  }
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] < array[i - 1]) {
-      assending = false;
-    }
-    if (array[i] > array[i - 1]) {
-      descending = false;
-    }
-  }
-  if (assending) return "sorted in assending order";
-  if (descending) return "sorted in descending order";
-  return "array do not have any order";
-};
-let check = isSorted([2, 1]);
-console.log(check);
+}
+console.log(even, "evens");
+console.log(odd, "odds");
