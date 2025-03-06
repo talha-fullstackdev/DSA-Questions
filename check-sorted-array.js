95c9ba02ee05223e2aa31618601f989de43d1027
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-06 15:56:15 -0800

    pratice is sorted array

--- check-sorted-array.js
+++ check-sorted-array.js
@@ -1,28 +1,51 @@
-const isSorted = (array) => {
-    if (array.length < 2) { 
-        return "Array is sorted"; 
-    }
-    let ascending = true;
-    let descending = true;
+// const isSorted = (array) => {
+//     if (array.length < 2) {
+//         return "Array is sorted";
+//     }
+//     let ascending = true;
+//     let descending = true;
 
-    for (let i = 1; i < array.length; i++) {
-        if (array[i] < array[i - 1]) {
-            ascending = false; 
-        }
-        if (array[i] > array[i - 1]) {
-            descending = false; 
-        }
-    }
-    if (ascending) return "Sorted in ascending order";
-    if (descending) return "Sorted in descending order";
-    return "Not sorted";
-}
+//     for (let i = 1; i < array.length; i++) {
+//         if (array[i] < array[i - 1]) {
+//             ascending = false;
+//         }
+//         if (array[i] > array[i - 1]) {
+//             descending = false;
+//         }
+//     }
+//     if (ascending) return "Sorted in ascending order";
+//     if (descending) return "Sorted in descending order";
+//     return "Not sorted";
+// }
 
-let array = [2, 5, 6, 7, 3, 5]; 
-console.log(isSorted(array)); 
+// let array = [2, 5, 6, 7, 3, 5];
+// console.log(isSorted(array));
 
 // let sortedArray = [1, 2, 3, 4, 5];
 // console.log(isSorted(sortedArray));
 
 // let descendingArray = [5, 4, 3, 2, 1];
 // console.log(isSorted(descendingArray));
+
+const issorted = (array) => {
+  if(array.length===0){
+    return "array is empty"
+  }
+  if (array.length < 2) {
+    return "array is sorted";
+  }
+  let ascending = true;
+  let decending = true;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > array[i - 1]) {
+      decending = false;
+    }
+    if (array[i] < array[i - 1]) {
+      ascending = false;
+    }
+  }
+  if (ascending) return "sorted in assending order";
+  if (decending) return "sorted in decending order order";
+};
+const check = issorted([]);
+console.log(check);
