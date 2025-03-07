62e19d47b4bbe7af4723db7f58309e78c970ca82
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-06 16:16:39 -0800

    practice iseven and odd

--- even-odd-array.js
+++ even-odd-array.js
@@ -1,13 +1,31 @@
-const array = [2,2,4,6,7,3,9,11,6,14,15,17,18];
-let even = 0;
-let odd = 0;
-for (let i = 0; i < array.length; i++) {
-  if (array[i] % 2 === 0) {
-    even += 1;
+// const array = [2, 2, 4, 6, 7, 3, 9, 11, 6, 14, 15, 17, 18];
+// let even = 0;
+// let odd = 0;
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] % 2 === 0) {
+//     even += 1;
+//   }
+//   if (array[i] % 2 != 0) {
+//     odd += 1;
+//   }
+// }
+// console.log("even =", even);
+// console.log("odd =", odd);
+
+const EvenOdd = (array) => {
+  let even = 0;
+  let odd = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      even += 1;
+    } else {
+      odd += 1;
+    }
   }
-  if (array[i] % 2 != 0) {
-    odd += 1;
-  }
-}
-console.log("even =",even)
-console.log("odd =",odd)
+  return { even, odd };
+};
+
+const array = [1, 2, 3, 4, 5];
+// const check = EvenOdd(array);
+const { even, odd } = EvenOdd(array);
+console.log("even = ", even, "odd = ", odd);
