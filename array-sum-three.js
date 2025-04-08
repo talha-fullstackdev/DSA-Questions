0a36984af77aecc18fe934810d9e2027d3c8c5b8
talha-fullstackdev <talhafullstack786@gmail.com>
2025-04-08 17:48:27 +0500

    updated the logic in array-sum-three.js

--- array-sum-three.js
+++ array-sum-three.js
@@ -1,13 +1,13 @@
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 23, 45, 12, 34, 65, 47, 87, 45];
-const target = 12;
+const target = 1270;
 let found = false;
-let iteration = 1;
+let iteration = 0;
 for (let i = 0; i < nums.length; i++) {
   for (let j = i + 1; j < nums.length; j++) {
     for (let k = j + 1; k < nums.length; k++) {
       if (nums[i] + nums[j] + nums[k] === target)
         console.log(
-          `iteration ${iteration++} the sum of ${nums[i]}, ${nums[j]} and ${
+          `iteration ${iteration+=1} the sum of ${nums[i]}, ${nums[j]} and ${
             nums[k]
           } is = ${target} `
         );
@@ -15,7 +15,7 @@ for (let i = 0; i < nums.length; i++) {
     }
   }
 }
-console.log("total valid matches", iteration - 1);
+console.log("total valid matches", iteration );
 if (!found) {
   console.log("no match found");
 }
