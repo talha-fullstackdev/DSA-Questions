a70054a6409ba66cea79939cb873c3208b78f287
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-12 12:00:00 +0500

    practice fibonacci-numbers

--- check.js
+++ check.js
@@ -1,4 +1,6 @@
-const array = [9, 8, 7, 6, 5, 3, 7, 8, 9, 5, 4, 3, 5, 7, 8, 9, 1, 4, 7, 8, 9, 3, 2, 6, 8, 4,];
+const array = [
+  9, 8, 7, 6, 5, 3, 7, 8, 9, 5, 4, 3, 5, 7, 8, 9, 1, 4, 7, 8, 9, 3, 2, 6, 8, 4,
+];
 let target = 9;
 for (let i = 0; i < array.length; i++) {
   for (let j = i + 1; j < array.length; j++) {
