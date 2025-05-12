192a0d4215e349551b36d2d4a28a5c63528b5b39
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-12 12:43:36 +0500

    updated

--- Sum-all.js
+++ Sum-all.js
@@ -1,12 +1,10 @@
-
 function sumAll() {
-    let total = 0;
-    for (let i = 0; i < arguments.length; i++) {
-        total += arguments[i];
-    }
-    return total;
+  let total = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    total += arguments[i];
+  }
+  return total;
 }
 
-console.log(sumAll(1, 2, 3, 4, 5,8)); 
-console.log(sumAll(10, 20)); 
-
+console.log(sumAll(1, 2, 3, 4, 5, 8));
+console.log(sumAll(10, 20));
