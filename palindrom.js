1f4beb1ce5f472e904796413f4bfee01a27523d1
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-12 12:26:13 +0500

    practice palindrom function

--- palindrom.js
+++ palindrom.js
@@ -46,18 +46,40 @@
 // const check = checkPalin("aba");
 // console.log(check);
 
-const checkPalindrom = (input) => {
+// const checkPalindrom = (input) => {
+//   let string = input.toString();
+//   let left = 0;
+//   let right = string.length - 1;
+//   while (left < right) {
+//     if (string[left] !== string[right]) {
+//       return "not palindrom";
+//     }
+//     left++;
+//     right--;
+//   }
+//   return "palindrom";
+// };
+// const check = checkPalindrom("aaabaaa")
+// console.log(check)
+//////////////////////////
+const isPalindrom = (input) => {
   let string = input.toString();
   let left = 0;
   let right = string.length - 1;
+  if (string.length === 0) {
+    return "please provide an input";
+  }
+  if (string.length === 1) {
+    return "one char is always a palindrom";
+  }
   while (left < right) {
     if (string[left] !== string[right]) {
-      return "not palindrom";
+      return "input is not palindrom";
     }
     left++;
     right--;
   }
   return "palindrom";
 };
-const check = checkPalindrom("aaabaaa")
-console.log(check)
+let test = isPalindrom(98);
+console.log(test);
