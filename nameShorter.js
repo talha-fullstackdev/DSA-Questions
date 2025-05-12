7f2d36a7c48037ca2063a237298e87ac0777fe47
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-12 12:07:47 +0500

    learn how to short a string using substring method in jS

--- nameShorter.js
+++ nameShorter.js
@@ -1,18 +1,21 @@
 // this function two argument one is name and other is its length and then works according to the condition
 
-const nameShortner = (name, maxLength) => {
-  if (name.length > maxLength) {
-    return name.substring(0, maxLength) + "...";
-  }
-//   return name;
-};
-const myName = nameShortner("talhahamzabajjaaduedhscb", 10);
-console.log(myName);
-// the substring() function is used to give string in  rang we want
-// let myName = "hamzanawazjdsncsn"
-const shortName1 = myName.substring(0,10)
-const shortName2 = myName.substring(5,10)
-console.log(shortName1)
-console.log(shortName2)
+// const nameShortner = (name, maxLength) => {
+//   if (name.length > maxLength) {
+//     return name.substring(0, maxLength) + "...";
+//   }
+// //   return name;
+// };
+// const myName = nameShortner("talhahamzabajjaaduedhscb", 10);
+// console.log(myName);
+// // the substring() function is used to give string in  rang we want
+// // let myName = "hamzanawazjdsncsn"
+// const shortName1 = myName.substring(0,10)
+// const shortName2 = myName.substring(5,10)
+// console.log(shortName1)
+// console.log(shortName2)
 // console.log(exactName)
+let name = "Talha Nawaz"
+let shortName = name.substring(0,5)
+console.log(shortName) 
 
